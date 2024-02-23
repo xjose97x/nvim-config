ab30111c6264cb876012d0897570d7df79959b0d
Jose Escudero <joseignacioescudero@gmail.com>
2024-02-23 18:19:16 -0500

    initial setup

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    'AndreM222/copilot-lualine',
+    config = function()
+      local lualine = require("lualine")
+      lualine.setup({
+        options = {
+            icons_enabled = true,
+            theme = cozynight,
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+            disabled_filetypes = {},
+            always_divide_middle = true
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff',
+                {
+                    'diagnostics',
+                    sources = { "nvim_diagnostic" },
+                    symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+                }
+            },
+            lualine_c = { 'filename' },
+            lualine_x = { 'copilot' ,'encoding', 'fileformat', 'filetype' }, -- I added copilot here
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' }
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { 'filename' },
+            lualine_x = { 'location' },
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {},
+        extensions = {}
+      })
+    end
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "catppuccin",
+        },
+      })
+    end,
+  }
+}
